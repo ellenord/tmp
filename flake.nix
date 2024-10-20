8efa05f1931a0b67560ab0fbf23def6bf8328c8e
Alistratenko Nikita <107148095+ellenord@users.noreply.github.com>
2024-10-20 11:41:38 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    keystone-flake.url = "https://glpat-21FzbkxQfazustGFxTyX:x-oauth-basic@gitlab.com/labmap/keystone/";
-    keystone-flake.rev = "8e5bf06c48b85f89fb3e7fdaf918d0de4b313e66";  
+    keystone-flake.url = "https://glpat-21FzbkxQfazustGFxTyX:x-oauth-basic@gitlab.com/labmap/keystone/-/archive/flake_dev/keystone-flake_dev.tar.gz";
+  #  keystone-flake.rev = "8e5bf06c48b85f89fb3e7fdaf918d0de4b313e66";  
   };
-
+  
 	outputs = { self, nixpkgs, keystone-flake, ... }: {
 		nixosConfigurations.your-hostname = nixpkgs.lib.nixosSystem {
 			system = "aarch64-linux";
