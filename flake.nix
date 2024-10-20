430cdbae95aa4a6fae0a9d7650147811144a3b68
Alistratenko Nikita <107148095+ellenord@users.noreply.github.com>
2024-10-20 14:42:15 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,20 +4,19 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     keystone-flake.url = "https://glpat-21FzbkxQfazustGFxTyX:x-oauth-basic@gitlab.com/labmap/keystone/-/archive/flake_dev/keystone-flake_dev.tar.gz";
-    keystone-flake.rev = "8e5bf06c48b85f89fb3e7fdaf918d0de4b313e66";  
+#    keystone-flake.rev = "8e5bf06c48b85f89fb3e7fdaf918d0de4b313e66";  
   };  # Добавление flake из GitLab
 
-  outputs = { self, nixpkgs, keystone-flake }: {
-    # Использование flake для сборки или настройки
-    nixosConfigurations = {
-      hostname = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        modules = [
-          keystone-flake.packages.default
-        ];
-      };
+  outputs = { self, nixpkgs, keystone-flake }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+      ];
     };
   };
-
 }
 
