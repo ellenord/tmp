198459f164d15be61248112d1c86ea266900de6d
Alistratenko Nikita <107148095+ellenord@users.noreply.github.com>
2024-10-20 14:17:04 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,20 +4,24 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     keystone-flake.url = "https://glpat-21FzbkxQfazustGFxTyX:x-oauth-basic@gitlab.com/labmap/keystone/-/archive/flake_dev/keystone-flake_dev.tar.gz";
-  #  keystone-flake.rev = "8e5bf06c48b85f89fb3e7fdaf918d0de4b313e66";  
+    keystone-flake.rev = "8e5bf06c48b85f89fb3e7fdaf918d0de4b313e66";  
+  };  # Добавление flake из GitLab
+
+  outputs = { self, nixpkgs, keystone-flake }: {
+    # Использование flake для сборки или настройки
+    nixosConfigurations = {
+      hostname = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          keystone-flake.packages.default
+        ];
+      };
+    };
   };
-  
-	outputs = { self, nixpkgs, keystone-flake, ... }: {
-		nixosConfigurations.your-hostname = nixpkgs.lib.nixosSystem {
-			system = "aarch64-linux";
-			modules = [
-				./hardware-configuration.nix
-				keystone-flake.nixosModules.keystoneModule
-			];
-			config = {
-				keystone.enable = true;  
-			};
-		};
-	};
+
+  # Пакеты, которые должны быть установлены на системе
+  environment.systemPackages = with pkgs; [
+    keystone-flake.packages.default
+  ];
 }
 
