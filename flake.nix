95393ea407be1c0e307791520ff5d3996e4f65ee
Alistratenko Nikita <107148095+ellenord@users.noreply.github.com>
2024-10-20 16:28:32 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,22 +1,22 @@
 {
   description = "NixOS configuration using flake";
-
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    keystone-flake.url = "https://glpat-21FzbkxQfazustGFxTyX:x-oauth-basic@gitlab.com/labmap/keystone/-/archive/flake_dev/keystone-flake_dev.tar.gz";
-#    keystone-flake.rev = "8e5bf06c48b85f89fb3e7fdaf918d0de4b313e66";  
-  };  # Добавление flake из GitLab
-
-  outputs = { self, nixpkgs, keystone-flake }@inputs: {
-    # Please replace my-nixos with your hostname
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
-      ];
+  };
+  outputs = {self, 
+             nixpkgs,
+             ...}@inputs:
+  let
+    system = "aarch64-linux";
+  in 
+  {
+    nixosConfigurations = {
+      nixos = inputs.nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs system; };
+        modules = [
+          /home/nixos/keystone/flake.nix
+        ];
+      };
     };
   };
 }
-
